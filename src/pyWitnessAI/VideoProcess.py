53bafb3ea99492e47bbea24668cec66ef13f537e
sboogert <stewart.boogert@rhul.ac.uk>
2023-01-18 21:44:59 +0000

    Fix up plotting issues and more control over functions

--- src/pyWitnessAI/VideoProcess.py
+++ src/pyWitnessAI/VideoProcess.py
@@ -3,21 +3,22 @@ from deepface import DeepFace as _DeepFace
 import matplotlib.pyplot as _plt
 import numpy as _np
 
-def videoAnalysis(inputVideoFile, referenceImage = '') :
+def videoAnalysis(inputVideoFile, referenceImage = '', plot = True) :
     noDetection = processVideoFrameByFrame(inputVideoFile,referenceImage)
     withDetection = processVideoFrameByFrame(inputVideoFile,referenceImage, enforceDetection=True)
 
-    _plt.plot(noDection)
-    _plt.plot(withDection)
+    if plot :
+        _plt.plot(noDetection)
+        _plt.plot(withDetection)
 
-    _plt.fill_between(_np.arange(0, len(noDetection)), 0, 1, noDetection >= 0, alpha=0.5, color='g')
-    _plt.ylim(0,1)
-    _plt.xlabel("Frame number")
-    _plt.ylabel("Similarity $1-d$")
+        _plt.fill_between(_np.arange(0, len(withDetection)), 0, 1, noDetection >= 0, alpha=0.5, color='g')
+        _plt.ylim(0,1)
+        _plt.xlabel("Frame number")
+        _plt.ylabel("Similarity $1-d$")
 
     return {'noDetection':noDetection,'withDetection':withDetection}
 
-def processVideoFrameByFrame(inputVideoFile, referenceImage = '', enforceDetection = False) :
+def processVideoFrameByFrame(inputVideoFile, referenceImage = '', enforceDetection = False, imshow = True) :
 
     dist = []
 
@@ -27,13 +28,19 @@ def processVideoFrameByFrame(inputVideoFile, referenceImage = '', enforceDetecti
         ret,frame = cap.read()
         frameFileName = "frame%d.jpg" % count
 
-        cv2.imshow('window-name', frame)
+        # display frame
+        if imshow :
+            cv2.imshow('window-name', frame)
+
+        # write frame to disk
         cv2.imwrite(frameFileName, frame)
 
+        # deep face match between video frame and reference image
         try :
             obj = _DeepFace.verify(frameFileName,
                                    referenceImage,
-                                   enforce_detection=enforceDetection, prog_bar=False);
+                                   enforce_detection=enforceDetection,
+                                   prog_bar=False);
             print(1-obj['distance'])
             dist.append(1-obj['distance'])
         except ValueError :
@@ -46,5 +53,4 @@ def processVideoFrameByFrame(inputVideoFile, referenceImage = '', enforceDetecti
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
-    #_plt.plot(dist)
     return _np.array(dist)
