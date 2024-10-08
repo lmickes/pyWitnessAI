be3c2d45e79bb665f817170f8245014d5366685f
FernandoRummi <752879139h@gmail.com>
2024-10-08 14:22:04 +0100

    Update Test.py

--- src/pyWitnessAI/Test.py
+++ src/pyWitnessAI/Test.py
@@ -3,32 +3,38 @@ from .VideoAnalyzer import *
 
 def test():
     #  Input the path and arguments needed
-    video_path = "E:/Project.Pycharm/FaceDetection/Face_detection/Materials/2faces.mp4"
+    video_path = "D:/MscPsy/Data/Colloff2021/Eyewitness video.wmv"
     #  cascade_path = 'E:/Project.Pycharm/FaceDetection/Face_detection/Models/haarcascade_frontalface_alt.xml'
     a = VideoAnalyzer(video_path)
     # lineup_number = 2
 
     mt = FrameAnalyzerMTCNN()
     op = FrameAnalyzerOpenCV()
-    a.add_analyzer(mt)
-    a.add_analyzer(op)
+    # a.add_analyzer(mt)
+    # a.add_analyzer(op)
+
+    cropper = FrameProcessorCropper(10, 500, 500, 10)
+    a.add_processor(cropper)
+
     display_processor = FrameProcessorDisplayer(box='True')
     a.add_processor(display_processor)
 
-    #  Load the lineup
-    lineup_loader = LineupLoader(directory_path='D:/MscPsy/Data/Colloff2021/TargetFace')
-    lineup = lineup_loader.load_image()
+    writer = FrameProcessorVideoWriter("D:/MscPsy/Data/Colloff2021/cropped")
+    a.add_processor(writer)
+    # #  Load the lineup
+    # lineup_loader = LineupLoader(directory_path='D:/MscPsy/Data/Colloff2021/TargetFace')
+    # lineup = lineup_loader.load_image()
+    #
+    # #  Comparison of lineup faces with faces in the video
+    # similarity_analyzer = SimilarityAnalyzer(lineup_faces=lineup, detector=mt)
+    # a.add_analyzer(similarity_analyzer)
 
-    #  Comparison of lineup faces with faces in the video
-    similarity_analyzer = SimilarityAnalyzer(lineup_faces=lineup, detector=mt)
-    a.add_analyzer(similarity_analyzer)
+    a.run(0, 10)
 
-    a.run(0, 130)
-
-    print(a.get_analysis_info())
-
-    a.save_data_flattened()
-
+    # print(a.get_analysis_info())
+    #
+    # a.save_data_flattened()
+    #
     plt.figure(figsize=(9, 6))
     plt.suptitle("Video analysis", fontsize=24, fontweight='bold')
     plt.subplot(2, 2, 1)
